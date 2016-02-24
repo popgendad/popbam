a6262fa6b5401c93bf489ad916c3b792935a9e16
Daniel Garrigan <dgarriga@lummei.net>
2016-02-24 15:25:37 -0500

    Added pop_fasta.cpp

---
+++ pop_fasta.cpp
@@ -0,0 +1,11 @@
+/** \file pop_fasta.cpp
+ *  \brief Functions for creating a multi-entry fasta from a BAM file
+ *  \author Daniel Garrigan
+ *  \version 0.5
+*/
+
+int
+mainFasta(int argc, char *argv[])
+{
+  return 0;
+}
